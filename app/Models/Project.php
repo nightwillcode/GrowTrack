4a81eaad8fc88227c1fbe3b18d196bebc5fcb7f5
Willy <dermawanwilly9@gmail.com>
2025-06-09 13:09:00 +0700

    Filament Setup

---
+++ app/Models/Project.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'user_id',
+    ];
+
+    protected static function booted(): void
+    {
+        static::creating(function ($project) {
+            if (!$project->id) {
+                $project->id = (string) Str::uuid();
+            }
+        });
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
